2d24d8b7571034f309382d4674fd21f86880eead
Hampus Rosvall <hampusrosvall@hr.lan>
2020-05-21 11:16:30 +0200

    added quick sort

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,159 +1,169 @@
-import React from 'react'; 
-import './SortingVisualizer.css'; 
-import heapSort from '../sortingAlgorithms/heapSort.js'; 
-import quickSort from '../sortingAlgorithms/quickSort.js'; 
+import React from "react";
+import "./SortingVisualizer.css";
+import heapSort from "../sortingAlgorithms/heapSort.js";
+import quickSort from "../sortingAlgorithms/quickSort.js";
 
-const LOWERBOUND = 5; 
-const UPPERBOUND = 300; 
-const SIZE = 60; 
-const STANDARD_COLOR = "paleturquoise"; 
-const COMPARING_COLOR = "lightcoral"; 
-const FINAL_COLOR = "lightgreen"; 
-const SPEED = 50; 
+const LOWERBOUND = 5;
+const UPPERBOUND = 300;
+const SIZE = 230;
+const STANDARD_COLOR = "paleturquoise";
+const COMPARING_COLOR = "lightcoral";
+const SPEED = 1;
+const ALTER_SPEED = 0.5;
 
 export default class SortingVisualizer extends React.Component {
-    constructor(props) {
-        super(props); 
-        this.state = {
-            array: [],
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      array: [],
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < SIZE; i++) {
+      array.push(generateRandomNumber(LOWERBOUND, UPPERBOUND));
     }
+    this.setState({ array: array });
 
-    componentDidMount() {
-        this.resetArray()
+    const arrayBars = document.getElementsByClassName("array-bar");
+    for (let bar of arrayBars) {
+      bar.style.backgroundColor = STANDARD_COLOR;
     }
+  }
 
-    resetArray(){
-        const array = []; 
-        for (let i = 0; i < SIZE; i++){
-            array.push(generateRandomNumber(LOWERBOUND, UPPERBOUND)); 
-        }
-        this.setState({array: array});
+  quickSort() {
+    const [comparisons, sortedArray] = quickSort(this.state.array.slice());
+    testQuickSort(sortedArray, this.state.array.slice());
+    const states = ["HIGHLIGHT", "SWAP_HEIGHTS", "DEHIGHLIGHT"];
+    let counts = 1;
+    for (let i = 0; i < comparisons.length; i++) {
+      const comparison = comparisons[i];
+      const history = comparison.history;
 
-        const arrayBars = document.getElementsByClassName("array-bar"); 
-        for (let bar of arrayBars){
-            bar.style.backgroundColor = STANDARD_COLOR; 
-        }
-    }
+      for (let j = 0; j < history.length; j++) {
+        const arrayBars = document.getElementsByClassName("array-bar");
+        const currentHistory = history[j];
+        const left = currentHistory.left;
+        const right = currentHistory.right;
+        const didSwap = currentHistory.swap;
 
-    quickSort(){
-        const comparisons = quickSort(this.state.array.slice());
-        const PIVOT_COLOR = "yellow"; 
-
-        let outerSpeed = 1; 
-        for (let comparison of comparisons){
-            const arrayBars = document.getElementsByClassName("array-bar")
-
+        for (let k = 0; k < states.length; k++) {
+          const currentState = states[k];
+          let color;
+          if (currentState === "HIGHLIGHT" || currentState === "DEHIGHLIGHT") {
+            color =
+              currentState === "HIGHLIGHT" ? COMPARING_COLOR : STANDARD_COLOR;
             setTimeout(() => {
-                let pivotIdx = comparison.pivotIdx; 
-                let pivotStyle = arrayBars[pivotIdx].style; 
-                pivotStyle.backgroundColor = PIVOT_COLOR; 
-            }, SPEED * outerSpeed); 
-            
-            let innerSpeed = 1; 
-            for (let history of comparison.history){
-                setTimeout(() => {
-                    let left = history.left; 
-                    let right = history.right; 
-                    let didSwap = history.swap; 
-                    let leftBarStyle = arrayBars[left].style; 
-                    let rightBarStyle = arrayBars[right].style; 
-
-                    leftBarStyle.backgroundColor = COMPARING_COLOR; 
-                    rightBarStyle.backgroundColor = COMPARING_COLOR;
-
-                    if (didSwap){
-                        const tmpHeight = leftBarStyle.height; 
-                        leftBarStyle.height = rightBarStyle.height; 
-                        rightBarStyle.height = tmpHeight; 
-
-                    }
-                }, SPEED * (outerSpeed + innerSpeed));
-                innerSpeed++; 
-            }
-            outerSpeed++; 
+              setComparingColors(left, right, arrayBars, color);
+            }, counts * ALTER_SPEED * SPEED);
+          } else if (currentState === "SWAP_HEIGHTS" && didSwap) {
+            setTimeout(() => {
+              updateBarHeights(left, right, arrayBars);
+            }, counts * ALTER_SPEED * SPEED);
+          }
+          counts++;
         }
+        counts++;
+      }
+      counts++;
     }
+  }
 
-    heapSort(){
-        const states = ["HIGHLIGHT", "SWAP_HEIGHTS", "DEHIGHLIGHT"]; 
-        const information = heapSort(this.state.array.slice());
-        for (let i = 0; i < information.length; i ++){
-            const elem = information[i]; 
-            const [firstIdx, secondIdx] = elem.comparing; 
-            const arrayBars = document.getElementsByClassName("array-bar"); 
+  heapSort() {
+    const states = ["HIGHLIGHT", "SWAP_HEIGHTS", "DEHIGHLIGHT"];
+    const information = heapSort(this.state.array.slice());
+    let counts = 1;
+    for (let i = 0; i < information.length; i++) {
+      const elem = information[i];
+      const [firstIdx, secondIdx] = elem.comparing;
+      const arrayBars = document.getElementsByClassName("array-bar");
 
-            for (let k = 0; k < states.length; k++){
-                const currentState = states[k];
-                if (currentState === "HIGHLIGHT" || currentState === "DEHIGHLIGHT") {
-                    let color; 
-                    if (elem.final){
-                        color = FINAL_COLOR;
-                    } else {
-                        color = currentState === "HIGHLIGHT" ? COMPARING_COLOR : STANDARD_COLOR;
-                    }
-                    setTimeout(() => {
-                        setComparingColors(firstIdx, secondIdx, arrayBars, color); 
-                        }, (i + k) * SPEED);
-                } else if (currentState === "SWAP_HEIGHTS" && elem.swap){
-                    setTimeout(() => {
-                        updateBarHeights(firstIdx, secondIdx, arrayBars, i);  
-                    }, (i + k) * SPEED);
-                }
-            }      
+      for (let k = 0; k < states.length; k++) {
+        const currentState = states[k];
+        if (currentState === "HIGHLIGHT" || currentState === "DEHIGHLIGHT") {
+          let color;
+          color =
+            currentState === "HIGHLIGHT" ? COMPARING_COLOR : STANDARD_COLOR;
+          setTimeout(() => {
+            setComparingColors(firstIdx, secondIdx, arrayBars, color);
+          }, counts * ALTER_SPEED * SPEED);
+        } else if (currentState === "SWAP_HEIGHTS" && elem.swap) {
+          setTimeout(() => {
+            updateBarHeights(firstIdx, secondIdx, arrayBars);
+          }, counts * ALTER_SPEED * SPEED);
         }
+        counts++;
+      }
+      counts++;
     }
+  }
 
-    render(){
-        const {array} = this.state; 
-        return (
-            <div classname="background">
-            <div className="array-container">
-            {array.map((value, index) => (
-                <div 
-                    className="array-bar"
-                    key={index}
-                    style={{height: `${value}px`}}>
-                </div>
-            ))}
-            </div>
-            <button className="button" onClick={() => this.resetArray()}>Generate new array</button>
-            <button className="button" onClick={() => this.heapSort()}>Heap Sort</button>
-            <button className="button" onClick={() => this.quickSort()}>Quick Sort</button>
-            </div>
-        )
-    }
+  render() {
+    const { array } = this.state;
+    return (
+      <div className="background">
+        <div className="array-container">
+          {array.map((value, index) => (
+            <div
+              className="array-bar"
+              key={index}
+              style={{ height: `${value}px` }}
+            ></div>
+          ))}
+        </div>
+        <button className="button" onClick={() => this.resetArray()}>
+          Generate new array
+        </button>
+        <button className="button" onClick={() => this.heapSort()}>
+          Heap Sort
+        </button>
+        <button className="button" onClick={() => this.quickSort()}>
+          Quick Sort
+        </button>
+      </div>
+    );
+  }
 }
 
-function setComparingColors(firstIdx, secondIdx, bars, color){
-    const firstBarStyle = bars[firstIdx].style; 
-    const secondBarStyle = bars[secondIdx].style;
-    firstBarStyle.backgroundColor = color;
-    secondBarStyle.backgroundColor = color;
+function setComparingColors(firstIdx, secondIdx, bars, color) {
+  const firstBarStyle = bars[firstIdx].style;
+  const secondBarStyle = bars[secondIdx].style;
+  firstBarStyle.backgroundColor = color;
+  secondBarStyle.backgroundColor = color;
 }
 
-function updateBarHeights(firstIdx, secondIdx, bars){
-    const firstBarStyle = bars[firstIdx].style; 
-    const secondBarStyle = bars[secondIdx].style;
-    const tmpHeight = firstBarStyle.height; 
-    firstBarStyle.height = secondBarStyle.height;
-    secondBarStyle.height = tmpHeight; 
+function updateBarHeights(firstIdx, secondIdx, bars) {
+  const firstBarStyle = bars[firstIdx].style;
+  const secondBarStyle = bars[secondIdx].style;
+  const tmpHeight = firstBarStyle.height;
+  firstBarStyle.height = secondBarStyle.height;
+  secondBarStyle.height = tmpHeight;
 }
 
-function generateRandomNumber(lowerBound, upperBound){
-    return Math.floor(Math.random() * (upperBound - lowerBound + 1) + lowerBound); 
+function generateRandomNumber(lowerBound, upperBound) {
+  return Math.floor(Math.random() * (upperBound - lowerBound + 1) + lowerBound);
+}
+
+function testQuickSort(sortedArray, unSortedArray) {
+  const buildInSort = unSortedArray.slice().sort((a, b) => a - b);
+  console.log(JSON.stringify(sortedArray) === JSON.stringify(buildInSort));
 }
 
 // function testSortingAlgorithm(sortingAlgo){
 //     for (let i = 0; i < 100; i++){
 //         const size = generateRandomNumber(1, 1000);
-//         let unSortedArray = []; 
+//         let unSortedArray = [];
 //         for (let j = 0; j < size; j++){
-//             unSortedArray.push(generateRandomNumber(1, 1000)); 
+//             unSortedArray.push(generateRandomNumber(1, 1000));
 //         }
 
-//         const sortedArray = sortingAlgo(unSortedArray.slice()); 
-//         const builtInSort = unSortedArray.slice().sort((a, b) => a - b); 
-//         console.log(JSON.stringify(sortedArray) === JSON.stringify(builtInSort)); 
+//         const sortedArray = sortingAlgo(unSortedArray.slice());
+//         const builtInSort = unSortedArray.slice().sort((a, b) => a - b);
+//         console.log(JSON.stringify(sortedArray) === JSON.stringify(builtInSort));
 //     }
-// } 
+// }
--- src/sortingAlgorithms/quickSort.js
+++ src/sortingAlgorithms/quickSort.js
@@ -1,48 +1,51 @@
 export const quickSort = (array) => {
-    let comparisons = []; 
-    quickSortHelper(0, array.length - 1, array, comparisons); 
-    return comparisons; 
-}; 
+  let comparisons = [];
+  quickSortHelper(0, array.length - 1, array, comparisons);
+  return [comparisons, array];
+};
 
-function quickSortHelper(startIdx, endIdx, array, comparisons){
-    if (startIdx >= endIdx) return; 
-    const pivotIdx = generatePivotIdx(startIdx, endIdx, "first"); 
+function quickSortHelper(startIdx, endIdx, array, comparisons) {
+  if (startIdx >= endIdx) {
+    return;
+  }
+  const pivotIdx = generatePivotIdx(startIdx, endIdx, "first");
 
-    let history = []; 
+  let history = [];
 
-    let left = startIdx, right = endIdx; 
-    while (left <= right){
-        let currentComp = {"left": left, "right": right, "swap": false}; 
-    
-        if (array[left] > array[pivotIdx] && array[right] < array[pivotIdx]){
-            swap(left, right, array); 
-            currentComp.swap = true; 
-        }
-        if (array[left] <= array[pivotIdx]) left++; 
-        if (array[right] >= array[pivotIdx]) right--; 
+  let left = startIdx + 1,
+    right = endIdx;
+  while (left <= right) {
+    let currentComp = { left: left, right: right, swap: false };
 
-        history.push(currentComp)
+    if (array[left] > array[pivotIdx] && array[right] < array[pivotIdx]) {
+      swap(left, right, array);
+      currentComp.swap = true;
     }
+    if (array[left] <= array[pivotIdx]) left++;
+    if (array[right] >= array[pivotIdx]) right--;
 
-    let finalSwap = {"left": pivotIdx, "right": right, "swap": true};
-    swap(right, pivotIdx, array); 
-    
-    history.push(finalSwap); 
-    let informationObject = {"pivotIdx": pivotIdx, "history": history}; 
-    comparisons.push(informationObject); 
+    history.push(currentComp);
+  }
 
-    quickSortHelper(startIdx, right - 1, array, comparisons); 
-    quickSortHelper(right + 1, endIdx, array, comparisons); 
+  const finalSwap = { left: pivotIdx, right: right, swap: true };
+  swap(right, pivotIdx, array);
+
+  history.push(finalSwap);
+
+  let informationObject = { pivotIdx: pivotIdx, history: history };
+  comparisons.push(informationObject);
+  quickSortHelper(startIdx, right - 1, array, comparisons);
+  quickSortHelper(right + 1, endIdx, array, comparisons);
 }
 
-function generatePivotIdx(startIdx, endIdx, strategy){
-    if (strategy === "first") return startIdx; 
+function generatePivotIdx(startIdx, endIdx, strategy) {
+  if (strategy === "first") return startIdx;
 }
 
-function swap(i, j, a){
-    const tmp = a[i]; 
-    a[i] = a[j]; 
-    a[j] = tmp; 
+function swap(i, j, a) {
+  const tmp = a[i];
+  a[i] = a[j];
+  a[j] = tmp;
 }
 
 export default quickSort;
